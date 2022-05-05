1d4378456b44ceeb52db652739cc4673b2fe7940
Gittoman <anonyme.and.free@protonmail.com>
2022-05-05 20:24:29 +0300

    HelloTriangleApplication class abstracted

--- src/Header.cpp
+++ src/Header.cpp
@@ -1,1 +1,88 @@
 #include "Header.h"
+
+void Test::print()
+{
+  doStuff();
+}
+
+void Test::doStuff()
+{
+  std::cout << "This is a test\n";
+}
+
+void HelloTriangleApplication::initWindow()
+{
+  glfwInit();
+
+  glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+  glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+
+  window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", nullptr, nullptr);
+}
+
+void HelloTriangleApplication::initVulkan()
+{
+  createInstance();
+}
+
+void HelloTriangleApplication::mainLoop()
+{
+  while (!glfwWindowShouldClose(window))
+  {
+    glfwPollEvents();
+  }
+}
+
+void HelloTriangleApplication::cleanup()
+{
+  vkDestroyInstance(instance, nullptr);
+
+  glfwDestroyWindow(window);
+  glfwTerminate();
+}
+
+void HelloTriangleApplication::createInstance()
+{
+  // some application info not necessarely have to have these but it is good practise
+  VkApplicationInfo appInfo{};
+  appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+  appInfo.pApplicationName = "Hello Triangle";
+  appInfo.applicationVersion = VK_MAKE_VERSION(1, 3, 0);
+  appInfo.pEngineName = "No Engine";
+  appInfo.engineVersion = VK_MAKE_VERSION(1, 3, 0);
+  appInfo.apiVersion = VK_API_VERSION_1_3;
+
+  // this struct is not optional it tells the vulkan driver which global extensions and validation layers we want to use
+  // global meaning they appy to  the entire program and not  a specific device
+  VkInstanceCreateInfo createInfo{};
+  createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+  createInfo.pApplicationInfo = &appInfo;
+
+  uint32_t glfwExtensionCount = 0;
+  const char **glfwExtensions;
+
+  glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+  createInfo.enabledExtensionCount = glfwExtensionCount;
+  createInfo.ppEnabledExtensionNames = glfwExtensions;
+
+  createInfo.enabledLayerCount = 0;
+
+  VkResult result = vkCreateInstance(&createInfo, nullptr, &instance);
+
+  if (vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS)
+  {
+    throw std::runtime_error("failed to create instance!");
+  }
+
+  uint32_t extensionCount = 0;
+  vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+  std::vector<VkExtensionProperties> extensions(extensionCount);
+  vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, extensions.data());
+
+  std::cout << "available extension\n";
+  for (const auto &extension : extensions)
+  {
+    std::cout << '\t' << extension.extensionName << '\n';
+  }
+}
--- src/Header.h
+++ src/Header.h
@@ -1,1 +1,45 @@
 #pragma once
+
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
+#include <iostream>
+#include <vector>
+
+class Test
+{
+public:
+  void print();
+
+private:
+  void doStuff();
+};
+
+class HelloTriangleApplication
+{
+private:
+  GLFWwindow *window;
+  const uint32_t WIDTH = 800;
+  const uint32_t HEIGHT = 600;
+  VkInstance instance;
+
+public:
+  void run()
+  {
+    initWindow();
+    initVulkan();
+    mainLoop();
+    cleanup();
+  }
+
+private:
+  void initWindow();
+
+  void initVulkan();
+
+  void mainLoop();
+
+  void cleanup();
+
+  void createInstance();
+};
